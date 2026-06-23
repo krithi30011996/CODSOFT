037a13c3700d3ed8e464c43596472259931e5801
krithi30011996 <kavipriyamagalingam@gmail.com>
2026-06-23 22:52:29 +0530

    message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './styles/home.css'
 import App from './App.jsx'
 import {Toaster} from "react-hot-toast";
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Toaster position='top-right'/>
+    <BrowserRouter>
+        <App />
+        <Toaster position='top-right'/>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
