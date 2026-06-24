1b8842506c123364c21055b2de7e0ad4078df692
krithi30011996 <kavipriyamagalingam@gmail.com>
2026-06-24 20:22:14 +0530

    update in register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,31 +3,98 @@ import { register } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 
 function Register() {
-    const navigate = useNavigate();
-    const [form, setForm] = useState({ name: "", email: "", password: "" });
-    async function submit() {
-        try {
-            await register({
-                name,
-                email,
-                password
-            });
-            alert("Registered Successfully");
-            navigate("/login");
-        }
-        catch(error) {
-            alert(error.message);
-        }
-    }
-    return (
-        <div className="create-page">
-            <h1>Register</h1>
-            <input placeholder="Name" onChange={(e) => setForm({ ...form, name: e.target.value })} />
-            <input placeholder="Email" onChange={(e) => setForm({ ...form, email: e.target.value })} />
-            <input type="password" placeholder="Password" onChange={(e) => setForm({ ...form, password: e.target.value })} />
-            <button onClick={submit}>Register</button>
-        </div>
-    );
+
+const navigate = useNavigate();
+
+const [form, setForm] = useState({
+name:"",
+email:"",
+password:""
+});
+
+async function submit(){
+
+try{
+
+await register(form);
+
+alert("Registered Successfully");
+
+navigate("/login");
+
+}
+
+catch(error){
+
+alert(
+error.response?.data?.message
+||
+"Register Failed"
+);
+
+}
+
+}
+
+return(
+
+<div className="create-page">
+
+<h1>
+
+Register
+
+</h1>
+
+<input
+placeholder="Name"
+value={form.name}
+onChange={(e)=>
+
+setForm({
+...form,
+name:e.target.value
+})
+
+}
+/>
+
+<input
+placeholder="Email"
+value={form.email}
+onChange={(e)=>
+
+setForm({
+...form,
+email:e.target.value
+})
+
+}
+/>
+
+<input
+type="password"
+placeholder="Password"
+value={form.password}
+onChange={(e)=>
+
+setForm({
+...form,
+password:e.target.value
+})
+
+}
+/>
+
+<button onClick={submit}>
+
+Register
+
+</button>
+
+</div>
+
+);
 
 }
 
